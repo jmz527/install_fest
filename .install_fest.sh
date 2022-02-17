3d4b223f9dfd5000ff2ad4a0db4d9b4f1560fe6f
james rutledge <jarutledge2@gmail.com>
2022-02-16 19:06:19 -0600

    modify .install_fest.sh: consolidate

--- .install_fest.sh
+++ .install_fest.sh
@@ -13,6 +13,9 @@
 # Set up basic env vars
 #-------------------------------------------------------------------------------
 
+# import utility methods
+. ./.utilities.sh
+
 # Standard Bash Variables
 # `OSTYPE` A string describing the operating system Bash is running on.
 # `MACHTYPE` system type in cpu-company-system
@@ -47,102 +50,6 @@ fi
 # SCRIPT_ROOT="$HOME/.code"
 
 # mkdir -pv "$SCRIPT_ROOT"
-
-
-
-#-------------------------------------------------------------------------------
-# Set text formatting
-#-------------------------------------------------------------------------------
-
-# set 256 color profile where possible
-if [[ $COLORTERM == gnome-* && $TERM == xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
-  export TERM=gnome-256color
-elif infocmp xterm-256color >/dev/null 2>&1; then
-  export TERM=xterm-256color
-fi
-
-# Reset formatting
-RESET=$(      tput sgr0)
-
-# Foreground color
-BLACK=$(      tput setaf 0)
-RED=$(        tput setaf 1)
-GREEN=$(      tput setaf 2)
-YELLOW=$(     tput setaf 3)
-BLUE=$(       tput setaf 4)
-MAGENTA=$(    tput setaf 5)
-CYAN=$(       tput setaf 6)
-WHITE=$(      tput setaf 7)
-
-# Background color
-BG_BLACK=$(   tput setab 0)
-BG_RED=$(     tput setab 1)
-BG_GREEN=$(   tput setab 2)
-BG_YELLOW=$(  tput setab 3)
-BG_BLUE=$(    tput setab 4)
-BG_MAGENTA=$( tput setab 5)
-BG_CYAN=$(    tput setab 6)
-BG_WHITE=$(   tput setab 7)
-
-# Style
-UNDERLINE=$(  tput smul)
-NOUNDERLINE=$(tput rmul)
-BOLD=$(       tput bold)
-ITALIC=$(     tput sitm)
-
-#-------------------------------------------------------------------------------
-# Logging (utils/log_file.sh)
-#-------------------------------------------------------------------------------
-
-# clear
-
-# # set up logfile
-# LOGFILE="$SCRIPT_ROOT/install.log"
-
-# exec > >(tee $LOGFILE); exec 2>&1
-
-# echo "Script compiled at: ${COMPILED_AT}"
-# echo "Script execution begun: $(date)"
-# echo ""
-
-# utils/log_screen.sh
-
-function show () {
-  echo -e "${WHITE}> $* ${RESET}"
-}
-
-function inform () {
-  if [[ $2 ]]; then echo ""; fi
-  echo -e "${GREEN}${BOLD}>>>>  $1 ${RESET}"
-}
-
-function warn () {
-  if [[ $2 ]]; then echo ""; fi
-  echo -e "${YELLOW}${BOLD}>>>>  $1 ${RESET}"
-}
-
-function fail () {
-  if [[ $2 ]]; then echo ""; fi
-  echo -e "${RED}${BOLD}>>>>  $1 ${RESET}"
-}
-
-function pause_awhile () {
-  if [[ $2 ]]; then echo ""; fi
-  echo -e "${YELLOW}${BOLD}>>>>  $1 ${RESET}"
-  read -p "${YELLOW}${BOLD}Press <Enter> to continue.${RESET}"
-}
-
-function pause_and_warn () {
-  if [[ $2 ]]; then echo ""; fi
-  echo -e "${YELLOW}${BOLD}>>>>  $1 ${RESET}"
-  echo -e "${YELLOW}${BOLD}>>>> ${RESET}"
-  read -p "${YELLOW}${BOLD}>>>>  Continue? [Yy] ${RESET} " -n 1 -r
-
-  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    fail "Exiting..." true
-    exit 1;
-  fi
-}
 
 #-------------------------------------------------------------------------------
 # We begin! (utils/script_instructions.sh)
